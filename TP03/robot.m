18c2b4ab1f3e43d6aee68212a33e276cd0cd464d
CamiCamFlor <campocamola@gmail.com>
2025-10-08 18:07:50 -0300

    Modificado TP3, defini matriz DH, y offsets

--- TP03/robot.m
+++ TP03/robot.m
@@ -1,21 +1,21 @@
 %% Definicion de los parametros del robot
-d_tool = 0.220; % Definición explícita del desplazamiento de la herramienta puede cambiar segun la herramienta
+d_tool = 0.0; % Definición explícita del desplazamiento de la herramienta puede cambiar segun la herramienta
 dh = [ ...
-    0       0.000   0.000    -pi/2   0;   % Joint 1
-    0       0.000   0.400     0      0;   % Joint 2
-    0       0.000   0.210    -pi/2   0;   % Joint 3
-    0       0.000   0.000     pi/2   0;   % Joint 4
-    0       0.000   0.000    -pi/2   0;   % Joint 5
-    0       0.220   0.000     0      0];  % Joint 6
+    0       0.283   0.000    -pi/2   0;   % Joint 1
+    0       0.000   0.398    0      0;   % Joint 2
+    0       0.000   0.213    0      0;   % Joint 3
+    0       0.000   0.000    pi/2   0;   % Joint 4
+    0       0.025   0.000    pi/2      0;   % Joint 5
+    0       0.166   0.000    0      0];  % Joint 6
 name = 'Robot Scan Arm';
 qlim = deg2rad([ ...
    -170  170;    % q1
-   -120  120;    % q2
+      0  240;    % q2
    -170  170;    % q3
    -180  180;    % q4
    -120  120;    % q5
    -360  360]);  % q6
-offset = deg2rad([0 -90 90 0 0 0]);
+offset = deg2rad([0 -270 0 0 0 0]);
 base = transl(0,0,0);
 tool = transl(d_tool,0,0);
 
